8bda6bf15117af234eeccf6e4b54e185a23a27b6
Navaneeth Venu <69888461+nauaneeth@users.noreply.github.com>
2021-08-18 20:38:36 +0530

    Add Themes

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+window.onload = function () {
+    setBar('linkedbb', 1);
+    setBar("twitterbb", 1);
+    setBar("instabb", 1);
+};
+
 function changePreview(status) {
     var p1 = document.getElementsByClassName("linkedinpostcontainer")[0];
     var p2 = document.getElementsByClassName("twitterpostcontainer")[0];
@@ -17,4 +23,50 @@ function changePreview(status) {
         case 3: p3.style.display = "flex"; i1.style.display = "flex"; break;
         case 4: p3.style.display = "flex"; i2.style.display = "flex"; break;
     }
+}
+
+function changeMode(status) {
+    // var p1 = document.getElementsByClassName("linkedinpostcontainer")[0];
+    // var p2 = document.getElementsByClassName("twitterpostcontainer")[0];
+    // var p3 = document.getElementsByClassName("instapostcontainer")[0];
+
+    // p1.style.display = "none";
+    // p2.style.display = "none";
+    // p3.style.display = "none";
+    // i1.style.display = "none";
+    // i2.style.display = "none";
+
+    // for (i = 1; i <= 4; ++i) {
+    //     document.getElementsByClassName("linkedbb")[0];
+    // }
+
+
+    if (status == 1 || status == 2) {
+        document.getElementsByClassName('text')[0].style.color = "#2B2B2B";
+        document.getElementsByClassName('text')[1].style.color = "#2B2B2B";
+        document.getElementsByClassName('text')[2].style.color = "#2B2B2B";
+    }
+    else {
+        document.getElementsByClassName('text')[0].style.color = "#ffffff";
+        document.getElementsByClassName('text')[1].style.color = "#ffffff";
+        document.getElementsByClassName('text')[2].style.color = "#ffffff";
+    }
+
+    setBar('linkedbb', status);
+    setBar("twitterbb", status);
+    setBar("instabb", status);
+
+    // switch (type) {
+    //     case 1: setBar("linkedbb", status); break;
+    //     case 2: setBar("twitterbb", status); break;
+    //     case 3: setBar("instabb"); break;
+    // }
+}
+
+function setBar(type, status) {
+    for (i = 0; i < 4; ++i) {
+        document.getElementsByClassName(type)[i].style.display = "none";
+    }
+
+    document.getElementsByClassName(type)[status - 1].style.display = "block";
 }
